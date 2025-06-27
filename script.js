071f6f602ffb2660cea7308a4e873620e76db2f7
William-F11 <williamfoyt@gmail.com>
2025-06-27 16:56:52 -0500

    Add rejected.png image to Media directory
    Added a correct and incorrect screen after a user answers a question

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const quizDisplay = document.getElementById('quizDisplay');
 const equationDisplay = document.getElementById('equationShow');
 const choice1Display = document.getElementById('button1');
 const choice2Display = document.getElementById('button2');
 const choice3Display = document.getElementById('button3');
 const choice4Display = document.getElementById('button4');
 const questionNumDisplay = document.getElementById('questionNumber');
+
+// Correct/Incorrect screens
+const resultsDisplay = document.getElementById('resultsDisplay');
+const correctDisplay = document.getElementById('correctDisplay');
+const incorrectDisplay = document.getElementById('incorrectDisplay');
+const questionResults = document.getElementById('questionResults');
+const yourAnsResults = document.getElementById('yourAnsResults');
+const correctAnsResults = document.getElementById('correctAnsResults');
 
 let equation = "";
 let answer = 0;
@@ -16,35 +25,45 @@ let totalQuestions = 0;
 let correct = false;
 
 function answerQuestion(choice) {
+    let answerChoiceValue = 0;
     if (choice == 1) {
         if (checkAnswer(choice1)) {
             correct = true;
         }
+        answerChoiceValue = choice1;
     }
     if (choice == 2) {
         if (checkAnswer(choice2)) {
             correct = true;
         }
+        answerChoiceValue = choice2;
     }
     if (choice == 3) {
         if (checkAnswer(choice3)) {
             correct = true;
         }
+        answerChoiceValue = choice3;
     }
     if (choice == 4) {
         if (checkAnswer(choice4)) {
             correct = true;
         }
+        answerChoiceValue = choice4;
     }
 
     // MAKE SOMETHING THAT SHOWS CORRECT/WRONG HERE
-
-    questionNum++;
-    if (questionNum > totalQuestions) {
-        // PLAY AGAIN SCREEN
+    quizDisplay.classList.add('hidden');
+    resultsDisplay.classList.remove('hidden');
+    if (correct) {
+        correctDisplay.classList.remove('hidden');
     } else {
-        createQuestion();
+        incorrectDisplay.classList.remove('hidden');
+
+        questionResults.textContent = equation;
+        yourAnsResults.textContent = answerChoiceValue;
+        correctAnsResults.textContent = answer;
     }
+    
 }
 
 function checkAnswer(ans) {
@@ -56,11 +75,12 @@ function checkAnswer(ans) {
 }
 
 function createQuestion() {
-    correct = false;
 
     equation = "" + Math.round(Math.random()*50) + "+" + Math.round(Math.random()*50);
     let choiceSpot = Math.floor((Math.random()*4)+1);
     answer = Math.round(eval(equation));
+
+    choice1, choice2, choice3, choice4 = 0;
 
     if (choiceSpot == 1) {
         choice1 = answer;
@@ -86,7 +106,8 @@ function createQuestion() {
         choice4 = Math.round(Math.random()*100);
     }
 
-    document.getElementById('choiceSpotDisplay').innerHTML = Number(choiceSpot);
+    // DEBUGGING: remove later
+    document.getElementById('choiceSpotDisplay').innerHTML = choiceSpot;
 
     updateDisplay();
 }
@@ -100,8 +121,27 @@ function updateDisplay() {
     questionNumDisplay.textContent = questionNum + "/" + totalQuestions;
 }
 
+function nextQuestion() {
+    questionNum++;
+    if (questionNum > totalQuestions) {
+        // GO TO THE START SCREEN AGAIN
+    } else {
+        createQuestion();
+        if (correct) {
+            resultsDisplay.classList.add('hidden');
+            correctDisplay.classList.add('hidden');
+        } else {
+            resultsDisplay.classList.add('hidden');
+            incorrectDisplay.classList.add('hidden');
+        }
+        quizDisplay.classList.remove('hidden');
+        correct = false;
+    }
+}
+
 function startGame(numOfQuestions) {
     questionNum = 1;
     totalQuestions = numOfQuestions;
     createQuestion();
+    quizDisplay.classList.remove('hidden');
 }
