932fecdb129edaf133415107aa483e66ce805a17
William-F11 <williamfoyt@gmail.com>
2025-07-02 20:00:19 -0500

    Add final grades screen and update layout for quiz results

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// TODO
+// Add division
+
+
 const quizDisplay = document.getElementById('quizDisplay');
 const equationDisplay = document.getElementById('equationShow');
 const choice1Display = document.getElementById('button1');
@@ -21,6 +25,12 @@ const numQuestionsSliderDisplay = document.getElementById('numQuestionsSliderDis
 const numQuestionsSlider = document.getElementById('numQuestionsSlider');
 const problemTypeDropdown = document.getElementById('problemTypeDropdown');
 
+// The Results Screen
+const finalGradeArea = document.getElementById('finalGradeArea');
+const finalGradeDisplay = document.getElementById('finalGradeDisplay');
+const finalCorrectDisplay = document.getElementById('finalCorrectDisplay');
+const finalIncorrectDisplay = document.getElementById('finalIncorrectDisplay');
+
 let equation = "";
 let answer = 0;
 let choice1 = 0;
@@ -32,7 +42,9 @@ let totalQuestions = 0;
 let correctQuestions = 0;
 let grade = 0;
 let correct = false;
+let problemType = "add";
 
+// Handles the buttons that let you answer
 function answerQuestion(choice) {
     let answerChoiceValue = 0;
     if (choice == 1) {
@@ -74,6 +86,7 @@ function answerQuestion(choice) {
     
 }
 
+// Checks the answer returns true or false
 function checkAnswer(ans) {
     if (ans == answer) {
         return true;
@@ -82,7 +95,8 @@ function checkAnswer(ans) {
     }
 }
 
-function createQuestion() {
+// Creates an addition question, then calls updateScreen()
+function createAdditionQuestion() {
 
     equation = "" + Math.round(Math.random()*50) + "+" + Math.round(Math.random()*50);
     let choiceSpot = Math.floor((Math.random()*4)+1);
@@ -120,6 +134,87 @@ function createQuestion() {
     updateDisplay();
 }
 
+// Creates an subtraction question, then calls updateScreen()
+function createSubtractionQuestion() {
+    let subNum2 = Math.round((Math.random()*50) + 50);
+    let subNum1 = Math.round((Math.random()*50) + subNum2);
+    equation = "" + subNum1 + "-" + subNum2;
+    let choiceSpot = Math.floor((Math.random()*4)+1);
+    answer = Math.round(eval(equation));
+
+    choice1, choice2, choice3, choice4 = 0;
+
+    if (choiceSpot == 1) {
+        choice1 = answer;
+    } else {
+        choice1 = Math.round(Math.random()*100);
+    }
+
+    if (choiceSpot == 2) {
+        choice2 = answer;
+    } else {
+        choice2 = Math.round(Math.random()*100);
+    }
+
+    if (choiceSpot == 3) {
+        choice3 = answer;
+    } else {
+        choice3 = Math.round(Math.random()*100);
+    }
+
+    if (choiceSpot == 4) {
+        choice4 = answer;
+    } else {
+        choice4 = Math.round(Math.random()*100);
+    }
+
+    // DEBUGGING: remove later
+    document.getElementById('choiceSpotDisplay').innerHTML = choiceSpot;
+
+    updateDisplay();
+}
+
+// Creates an subtraction question, then calls updateScreen()
+function createMultiplicationQuestion() {
+    let subNum2 = Math.round((Math.random()*50));
+    let subNum1 = Math.round((Math.random()*50));
+    equation = "" + subNum1 + "*" + subNum2;
+    let choiceSpot = Math.floor((Math.random()*4)+1);
+    answer = Math.round(eval(equation));
+
+    choice1, choice2, choice3, choice4 = 0;
+
+    if (choiceSpot == 1) {
+        choice1 = answer;
+    } else {
+        choice1 = Math.round(Math.random()*1000);
+    }
+
+    if (choiceSpot == 2) {
+        choice2 = answer;
+    } else {
+        choice2 = Math.round(Math.random()*1000);
+    }
+
+    if (choiceSpot == 3) {
+        choice3 = answer;
+    } else {
+        choice3 = Math.round(Math.random()*1000);
+    }
+
+    if (choiceSpot == 4) {
+        choice4 = answer;
+    } else {
+        choice4 = Math.round(Math.random()*1000);
+    }
+
+    // DEBUGGING: remove later
+    document.getElementById('choiceSpotDisplay').innerHTML = choiceSpot;
+
+    updateDisplay();
+}
+
+// Updates the display while playing
 function updateDisplay() {
     equationDisplay.textContent = equation;
     choice1Display.textContent = choice1;
@@ -130,23 +225,41 @@ function updateDisplay() {
     quizGrade.textContent = grade;
 }
 
+// Updates the start screen
 function updateStartPage() {
     numQuestionsSliderDisplay.textContent = numQuestionsSlider.value;
 }
 
+// Updates the ending screen
+function updateEndPage() {
+    finalGradeDisplay.textContent = (grade + "%");
+    finalCorrectDisplay.textContent = correctQuestions
+    finalIncorrectDisplay.textContent = ((totalQuestions)  - correctQuestions);
+}
+
+// Takes you off of the correct/incorrect screen
 function nextQuestion() {
     if (questionNum >= totalQuestions) {
         if (correct) {
             resultsDisplay.classList.add('hidden');
             correctDisplay.classList.add('hidden');
+            correctQuestions++;
         } else {
             resultsDisplay.classList.add('hidden');
             incorrectDisplay.classList.add('hidden');
         }
 
-        startPage.classList.remove('hidden');
+        updateEndPage();
+        finalGradeArea.classList.remove('hidden');
     } else {
-        createQuestion();
+        if (problemType == "add") {
+            createAdditionQuestion();
+        } else if (problemType == "sub") {
+            createSubtractionQuestion()
+        } else if (problemType == "mult") {
+            createMultiplicationQuestion()
+        }
+
         if (correct) {
             resultsDisplay.classList.add('hidden');
             correctDisplay.classList.add('hidden');
@@ -163,16 +276,32 @@ function nextQuestion() {
     updateDisplay();
 }
 
+// Takes you back to the start screen while playing
 function playAgain() {
     quizDisplay.classList.add('hidden');
     startPage.classList.remove('hidden');
 }
 
+// Lets you play again from the final screen
+function playAgainEnd() {
+    finalGradeArea.classList.add('hidden');
+    startPage.classList.remove('hidden');
+}
+
+// Starts the game
 function startGame() {
     questionNum = 1;
     correctQuestions = 0;
     totalQuestions = numQuestionsSlider.value;
-    createQuestion();
+    problemType = problemTypeDropdown.value;
+
+    if (problemType == "add") {
+        createAdditionQuestion();
+    } else if (problemType == "sub") {
+        createSubtractionQuestion();
+    } else if (problemType == "mult") {
+        createMultiplicationQuestion();
+    }
     quizDisplay.classList.remove('hidden');
     startPage.classList.add('hidden');
 
